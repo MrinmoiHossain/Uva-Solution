4307aaf966883cd40bb5e7f96c67e74c2bf528a8
MrinmoiHossain <mokit.aust@gmail.com>
2017-09-29 11:48:21 +0600

    algebra

--- C++/uva11547.cpp
+++ C++/uva11547.cpp
@@ -1,24 +1,17 @@
 #include <bits/stdc++.h>
-typedef long long int ll;
-#define read() freopen("input.txt", "r", stdin)
 using namespace std;
 
 int main(void)
 {
-	int T;
-	cin >> T;
+	int t;
+	cin >> t;
 
-	for(int i = 0, n; i < T; i++){
+	for(int i = 0, n; i < t; i++){
 		cin >> n;
 
-		ll a = (((n * 567) / 9) + 7492) * 235;
-		a /= 47;
-		a -= 498;
+		int digit = 315 * n + 36962;
 
-		int p = abs(a % 100);
-		int q = p % 10;
-
-		cout << (p - q) / 10 << endl;
+		cout << abs((digit / 10) % 10) << endl;
 	}
 
 	return 0;
